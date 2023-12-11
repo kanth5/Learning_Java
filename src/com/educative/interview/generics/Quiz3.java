8d3a229ca2023172b766ae03fa81ef02404f7343
kanth5 <mrajk5@gmail.com>
2023-12-11 18:19:27 +0530

    2 quizzes in generics

--- src/com/educative/interview/generics/Quiz3.java
+++ src/com/educative/interview/generics/Quiz3.java
@@ -1,2 +1,22 @@
-package com.educative.interview.generics;public class Quiz3 {
+package com.educative.interview.generics;
+
+// The Collection<?> is a collection of unknowns, and we can’t add any type to it. We don’t know what the element type of myColl stands for therefore
+// we cannot add objects to it. The only element that you can ever insert into a List<?> is null. The wildcard type prevents us from extracting elements
+// from the List<?> as any type other than Object.
+
+import java.util.*;
+
+class Quiz3 {
+    public static void main( String args[] ) {
+        Collection<?> myColl = new ArrayList<>();
+        // compile time error
+        // myColl.add(new Object());    // java: incompatible types: java.lang.Object cannot be converted to capture#1 of ?
+        myColl.add(null);
+        myColl.add(null);
+        System.out.println(myColl.isEmpty());
+        System.out.println(myColl);
+        System.out.println(myColl.size());
+        myColl.forEach(System.out::println);
+        System.out.println(myColl.getClass());
+    }
 }
--- src/com/educative/interview/generics/Quiz4.java
+++ src/com/educative/interview/generics/Quiz4.java
@@ -1,4 +1,25 @@
 package com.educative.interview.generics;
 
-public class Quiz4 {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class Quiz4 {
+    public static void main(String args[]) {
+        List<Object> list = new ArrayList<>();
+        list.add(new Integer(5));
+        list.add(new Double(77.23));
+        list.add("randomString");
+        notVeryUsefulMethod(list);
+    }
+
+    static void notVeryUsefulMethod(Collection<?> collection) {
+        for (Object obj : collection) {
+            System.out.println(obj.getClass());
+            System.out.println(obj.toString());
+        }
+    }
 }
+
+// The above method compiles and is being passed in a collection of type unknown. When we loop over the elements of the collection, we can only
+// invoke the methods of the Object class on them.
